aeffe1f6080b4d3a7b9dbc56450323456c2e0a19
takamoto0609 <takamoto0609@gamil.com>
2020-11-07 14:10:30 +0900

    Add for deploy by capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,11 +7,20 @@ set :application, 'light-house-osaka'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takamoto0609/light-house-osaka.git'
 
+# deploy先のディレクトリ。 
+set :deploy_to, '/var/www/light-house-osaka'
+
+# シンボリックリンクをはるファイル
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
+
+#出力するログのレベル
+set :log_level, :debug
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
@@ -27,7 +36,37 @@ set :keep_releases, 5
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
   end
 end
